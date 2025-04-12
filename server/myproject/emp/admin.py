1cdce3648c029e55e573ccb1a7655478347c9116
gajendra <davegk59@gmil.com>
2025-04-12 11:47:27 +0530

    added in emp views and create superuser and home

--- server/myproject/emp/admin.py
+++ server/myproject/emp/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from .models import Emp,Testimonial,Feedback 
 # Register your models here.
+
+class EmpAdmin(admin.ModelAdmin):
+    list_display=('name','working','emp_id','phone')
+    list_editable=('working','emp_id')
+    search_fields=('name',)
+    list_filter=('working',)
+    list_per_page=4
+
+
+admin.site.register(Emp,EmpAdmin)
